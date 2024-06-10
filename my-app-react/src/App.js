8fe37bec1d5350f7e8d5691dc0db3f1236b322bd
M1rell Jordão <mirellajordao4@gmail.com>
2024-06-10 17:39:32 +0000

    Projeto react componentes

---
+++ my-app-react/src/App.js
@@ -0,0 +1,23 @@
+export default function App(){
+
+  const message=<h1>Atribuição da tag html a uma variavel</h1>
+  function sum(a,b){
+    return a+b;
+  }
+
+
+  return(
+    <div className="App">
+    <header className="App-header">
+      {message}
+
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <p>
+        O resultado da soma é:{sum(5,5)}
+      </p>
+    </header>
+    </div>
+  )
+}
